d0196ab3442603d62075648f90e25cb9738a9f83
Simas <simonas@digitouch.lt>
2019-05-14 16:03:52 +0300

    add Create name View

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Welcome from './views/Welcome.vue';
+import ChatView from '@/views/ChatView.vue';
 
 Vue.use(Router);
 
@@ -10,16 +11,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Welcome',
+      component: Welcome,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/chat',
+      name: 'Chat',
+      component: ChatView,
+      props: true,
     },
   ],
 });
---
+++ src/views/ChatView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="chat container">
+    <h2>Hi, {{name}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ChatView',
+  props: ['name'],
+};
+</script>
