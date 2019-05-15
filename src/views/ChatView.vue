99659ed4631ced3b45506d3cbd0f02b864b77300
Simas <simonas@digitouch.lt>
2019-05-15 07:58:54 +0300

    add fetching and dynamic display of messages

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -4,10 +4,10 @@
     <div class="card">
       <div class="card-content">
         <ul class="messages">
-          <li class="message">
-            <span class="teal-text">Name</span>
-            <span class="grey-text text-darken-3">message content</span>
-            <span class="grey-text time">time</span>
+          <li class="message" v-for="message in messages" :key="message.id">
+            <span class="teal-text">{{message.name}}</span>
+            <span class="grey-text text-darken-3">{{message.content}}</span>
+            <span class="grey-text time">{{message.timestamp}}</span>
           </li>
         </ul>
       </div>
@@ -18,13 +18,36 @@
   </div>
 </template>
 <script>
+import db from '@/firebase/init';
+
 import NewMessage from '@/components/NewMessage.vue';
 
 export default {
   name: 'ChatView',
   props: ['name'],
+  data() {
+    return {
+      messages: [],
+    };
+  },
   components: {
     NewMessage,
+  },
+  created() {
+    const ref = db.collection('messages').orderBy('timestamp');
+    ref.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((element) => {
+        if (element.type === 'added') {
+          const { doc } = element;
+          this.messages.push({
+            id: doc.id,
+            name: doc.data().name,
+            content: doc.data().content,
+            timestamp: doc.data().timestamp,
+          });
+        }
+      });
+    });
   },
 };
 </script>
